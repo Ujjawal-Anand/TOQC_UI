1f14e183a1ceb8e66e0fbcd6ae7e7d5be38b368c
andy1729 <ujjawalanand1729@gmail.com>
2016-12-02 23:10:33 +0530

    created CategoryModel

---
+++ app/src/main/java/com/example/android/toqc/models/ShowCategory.java
@@ -0,0 +1,35 @@
+package com.example.android.toqc.models;
+
+/**
+ * Created by ujjawal on 2/12/16.
+ */
+
+public class ShowCategory {
+    private String text;
+    private int thumbnail;
+
+    public ShowCategory() {
+
+    }
+
+    public ShowCategory(String text, int thumbnail) {
+        this. text = text;
+        this.thumbnail = thumbnail;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(int thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
