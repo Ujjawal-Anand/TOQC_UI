463cafe41439a09bf6d5c5a8dae91534ce56f180
andy1729 <ujjawalanand1729@gmail.com>
2016-12-02 23:38:51 +0530

    created ShowCategoryFragment

--- app/src/main/java/com/example/android/toqc/MainActivity.java
+++ app/src/main/java/com/example/android/toqc/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.toqc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
