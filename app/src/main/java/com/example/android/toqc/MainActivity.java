c51361e68134129eb9f759220508d170b584d928
andy1729 <ujjawalanand1729@gmail.com>
2016-12-03 00:00:25 +0530

    added toolbar

--- app/src/main/java/com/example/android/toqc/MainActivity.java
+++ app/src/main/java/com/example/android/toqc/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.toqc;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.example.android.toqc.Fragments.ShowCategoryFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.main_content, new ShowCategoryFragment())
+                    .commit();
+        }
+    }
+
+       private void initToolbar() {
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
